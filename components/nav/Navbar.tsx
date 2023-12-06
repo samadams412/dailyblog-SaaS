1490483a190fd815a34e70bb759d91c6f83eefa6
Samuel Adams <samueladams412@gmail.com>
2023-12-05 23:17:16 -0600

    Setup GitHub login with OAuth using Supabase

---
+++ components/nav/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import { SiGithub } from "react-icons/si";
+import LoginForm from "./LoginForm";
+
+export default function Navbar() {
+    return (
+        <nav className="flex items-center justify-between">
+        <div className="group">
+            <Link href="/" className="text-2xl font-bold">DailyBlog</Link>
+            <div className="h-1 w-0 group-hover:w-full transition-all bg-red-500"></div>
+        </div>
+        <LoginForm/>
+        </nav>
+    );
+}
