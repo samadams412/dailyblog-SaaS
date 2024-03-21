fb2f7e67f2acaa3aa1950f174acd7e61e6a05f39
Samuel Adams <samueladams412@gmail.com>
2024-03-20 20:56:35 -0500

    Fixed responsiveness for home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default async function page() {
 	const { data: blogs } = await readBlog();
 
 	return (
-		<div className="w-full grid grid-cols-1 md:grid-cols-3 gap-5 p-5 xl:p-0">
+		<div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 p-5 xl:p-0">
 			{blogs?.map((blog, index) => {
 				return (
 					<Link
 						key={index}
 						href={"/blog/" + blog.id}
-						className="w-full border rounded-md bg-graident-dark p-5 hover:ring-2 ring-blue-500 transition-all cursor-pointer space-y-5 first:lg:col-span-2 first:md:col-span-3"
+						className="w-full border rounded-md bg-graident-dark p-5 hover:ring-2 ring-blue-500 transition-all cursor-pointer space-y-5 first:lg:col-span-2 first:xl:col-span-3"
 					>
 						<div className="relative w-full h-72 md:h-64 xl:h-96">
 							<Image
